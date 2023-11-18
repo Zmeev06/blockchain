995b77fc57894ddad2f77fc36d8f81c717377f62
Sergey Timoshenko <sergey.umk06@gmail.com>
2023-11-18 18:12:14 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { app } from './app/index';
 import './style.css';
 // import "primevue/resources/themes/lara-light-teal/theme.css";
 // import 'primevue/resources/themes/saga-orange/theme.css'; // Выберите нужный вам темный стиль
-import './app/styles/theme-2.css';
+import './app/styles/theme-2.css'
 
 import 'primevue/resources/primevue.min.css';
 
