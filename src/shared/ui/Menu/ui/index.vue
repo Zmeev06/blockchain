2a3aff96529f81a1195cd6d34368dd32419d8421
Sergey Timoshenko <sergey.umk06@gmail.com>
2023-11-18 21:58:45 +0300

    feat: add menu

---
+++ src/shared/ui/Menu/ui/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+interface MenuProps {
+  name: string;
+}
+
+const props = defineProps<MenuProps>();
+
+const sections = [
+  {
+    label: props.name,
+    class: "py-3 px-3 border-b border-grey text-[#4b4b4b]",
+  },
+  {
+    label: 'Настройки',
+    class: "py-2 px-3 hover:bg-grey text-dark",
+  },
+  {
+    label: 'Выйти',
+    class: "py-2 px-3 hover:bg-grey text-[#FF1717]",
+  },
+];
+</script>
+<template lang="html">
+  <div class="bg-[#f6f6f6] rounded-[5px] w-[150px] flex flex-col gap-[5px]">
+    <div v-for="(item, index) in sections" :key="index" :class="item.class">
+      <p>{{item.label}}</p>
+    </div>
+  </div>
+</template>
+<style lang="scss"></style>
--- src/widgets/WalletTop/ui/index.vue
+++ src/widgets/WalletTop/ui/index.vue
@@ -1,12 +1,29 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { Avatar } from "../../../shared/ui/Avatar";
+import { Menu } from "../../../shared/ui/Menu";
+import { onClickOutside } from "@vueuse/core";
 
 const name = ref("Sergey_T06");
+
+const isOpenMenu = ref(false);
+
+const menu = ref();
+
+onClickOutside(menu, () => (isOpenMenu.value = false));
 </script>
 <template lang="html">
   <div class="flex w-full justify-between items-center">
-    <Avatar :name="name" />
+    <div class="relative">
+      <Avatar :name="name" @click="isOpenMenu = true"/>
+      <Menu
+        name="Sergey_T06"
+        v-show="isOpenMenu"
+        class="absolute top-12 left-3"
+        ref="menu"
+      />
+    </div>
+
     <div class="w-fit flex flex-col items-center">
       <h2 class="w-fit text-[24px] text-white">Кошелек</h2>
       <p class="w-fit text-[20px] text-grey">{{ name }}</p>
